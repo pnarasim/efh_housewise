75305c3b435e1feb491b7dcf838a54d590c896c5
pnarasim <priya.narasimhan@gmail.com>
2017-08-07 21:25:15 +0530

    missed these files in the previous commit. same msg

--- edison/mysql_test/post_sensor_value.py
+++ edison/mysql_test/post_sensor_value.py
@@ -5,6 +5,9 @@ import mysql.connector
 from datetime import date, datetime, timedelta
 import ConfigParser
 import sys
+import subprocess
+import string
+import re
 
 add_reading= ("INSERT INTO hw_data " 
                 " (house_id, sensor_id, sensor_value, timeofpost) " 
@@ -12,10 +15,52 @@ add_reading= ("INSERT INTO hw_data "
 #data_reading = (house_id, '2', '33.33', datetime.now().date())
 
 config = ConfigParser.ConfigParser()
+noise_level=0
+co2_level=0
+hw_str = ''
+noise_level_id = ''
+co2_level_id = ''
+
+def scan_and_get_sensor_readings(hw_str):
+    noise_level=0
+    co2_level=0
+    f = open("bleout.txt", "w")
+    subprocess.call(["blescan", "-n"], stdout=f)
+    f.close()
+    infile=open("bleout.txt", "r")
+    for line in infile:
+        words= line.partition(":")
+        #print (words[0], " = ", words[2])
+        if ((words[2].find(hw_str)) != -1):
+        	print ("found our device, get sensor values")
+        	print (words[2])
+        	# get the next 2 lines, they have the sensor values
+            	s1 = infile.next()
+            	s2 = infile.next()
+            	words = s1.partition(":")
+            	if (words[0].find("0x22") != -1):
+                	sensor_level = words[2]
+                	noise_level = re.sub('[\W]+', '', sensor_level)
+            	words = s2.partition(":")
+            	if (words[0].find("0x33") != -1):
+                	sensor_level = words[2]
+            		co2_level = re.sub('[\W]+', '', sensor_level)
+            	print ("noise = ", noise_level)
+            	print ("co2 = ", co2_level)
+                return noise_level, co2_level
+                break
+        #print words
+    infile.close()
 
 def main():
     config.read(sys.argv[1])    
+    
     house_id = config.get('Default', 'house_id')
+    noise_level_id = config.get('Default', 'noise_level_id')
+    co2_level_id = config.get('Default', 'co2_level_id')
+    ble_str = config.get('Default', 'ble_string')
+    hw_str = ble_str + house_id
+    print (hw_str)
     db_host = config.get('Default', 'host')
     db_port = config.get('Default', 'port')
     db_user = config.get('Default', 'user')
@@ -25,10 +70,16 @@ def main():
     cnx = mysql.connector.connect(host=db_host, port=db_port, user=db_user,passwd=db_passwd, database=db_name)
     cursor = cnx.cursor()
     #test
-    show_dbs(cursor)
+    #show_dbs(cursor)
     # get data and post it to the table
+    noise_level, co2_level = scan_and_get_sensor_readings(hw_str)
+    n = int(noise_level, 16)
+    c = int(co2_level, 16)
+    data_reading = (house_id, noise_level_id, n, datetime.now().date())
+    cursor.execute(add_reading, data_reading)
+    data_reading = (house_id, co2_level_id, c, datetime.now().date())
 
-    #cursor.execute(add_reading, data_reading)
+    cursor.execute(add_reading, data_reading)
     #print_table("hw_data")
     cnx.commit()
     cursor.close()
@@ -41,18 +92,6 @@ def show_dbs(cursor):
     while row is not None:
         print(row)
         row = cursor.fetchone()
-
-#tbd : fix this syntax
-def print_table( tablename ):
-    "This prints the rows in this table"
-    query = ("select * from "
-            " (%s) " )
-    cursor.execute(query, tablename)
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
-
 
 
 
